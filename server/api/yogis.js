90e554c167335b582389fc4b6585a36e4f4e97e2
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-22 13:24:55 -0400

    feat: create GET all yogis server route

---
+++ server/api/yogis.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const {Yogi} = require('../db/models')
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  Yogi.findAll()
+    .then(yogis => res.json(yogis))
+    .catch(next)
+})
