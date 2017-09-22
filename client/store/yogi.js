a052df3b1e8eaf70103c17e3d9b5ec33d00e7325
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-22 14:59:18 -0400

    feat: create yogi reducer

---
+++ client/store/yogi.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import history from '../history'
+
+//ACTION TYPES
+const INITIALIZE = 'INITIALIZE_YOGIS'
+
+//ACTION CREATORS
+const init = yogis => ({type: INITIALIZE, yogis})
+
+//THUNKS
+export const fetchYogis = () => dispatch => {
+  axios.get('/api/yogis')
+       .then(res => dispatch(init(res.data)))
+       .catch(console.error)
+}; 
+
+//REDUCER
+export default function reducer (yogis = [], action) {
+  switch (action.type){
+    
+    case INITIALIZE:
+      return action.yogis
+
+    default:
+      return yogis    
+  }
+}  
