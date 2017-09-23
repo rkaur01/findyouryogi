83f73ca0185f7e81659acec02d372a11cfdfd237
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-23 16:11:37 -0400

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '463616636099-v5pomkroaoi75q1jtl9ljrijejfrglfg.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'pSAnD-PgJmpwFWa2G9W2oDjH'
-process.env.SESSION_SECRET = 'skeleton in the closer'
-process.env.CLARIFAI_SECRET = 'f541487887234840afa68744d48f4ade'
